afdaf39ea08af8e7fbcadbc9e70bda50d18a00e2
Philipp Metzler <phil@goli.at>
2016-05-14 18:36:35 +0200

    added immutable

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -3,23 +3,31 @@ import {IntlProvider} from 'react-intl'
 import React, {PropTypes} from 'react'
 
 const Provider = ({store, ...props}) => {
-  return (
-    <ReduxProvider store={store}>
-      <IntlProvider {...props} />
-    </ReduxProvider>
-  )
+    return (
+        <ReduxProvider store={store}>
+            <IntlProvider {...props} />
+        </ReduxProvider>
+    )
 }
 
 Provider.propTypes = {
-  children: PropTypes.element.isRequired,
-  store: PropTypes.object.isRequired,
+    children: PropTypes.element.isRequired,
+    store: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = (state) => {
-  const {intl} = state
-  return {
-    ...intl,
-  }
+/*
+    Alternative:
+    const {intl} = state
+    return {
+        locale: state.getIn(["intl", "locale"]),
+        messages: state.getIn(["intl", "messages"]),
+    }
+*/
+    const {intl} = state.toJS()
+    return {
+        ...intl,
+    }
 }
 
 export default connect(mapStateToProps)(Provider)
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
+import { fromJS } from "immutable";
+
 export Provider from './components/Provider'
 
 export const UPDATE = '@@intl/UPDATE'
 
 export const update = ({locale, messages}) => {
-  return {
-    type: UPDATE,
-    payload: {locale, messages},
-  }
+    return {
+        type: UPDATE,
+        payload: {locale, messages},
+    }
 }
 
-const initialState = {
-  locale: 'en',
-  messages: {},
-}
+const initialState = fromJS({
+    locale: 'en',
+    messages: {},
+})
 
 export function intlReducer(state = initialState, action) {
-  if (action.type !== UPDATE) {
-    return state
-  }
+    if (action.type !== UPDATE) {
+        return state
+    }
 
-  return { ...state, ...action.payload }
+    return state
+        .set("locale", action.payload.locale)
+        .set("messages", action.payload.messages)
 }
